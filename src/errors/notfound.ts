077f8cf561ba6d2c1bea9ec68bd3284a572b6882
Alexandr Musikhin <tophackr@icloud.com>
2024-07-06 12:15:50 +0300

    add class for unknown channel

--- src/errors/notfound.ts
+++ src/errors/notfound.ts
@@ -1,34 +1,34 @@
 import {RepliableInteraction} from 'discord.js'
 import {CustomError} from './general.js'
 
-abstract class NotFoundError extends CustomError {
-    public name = 'NotfoundError'
-}
+abstract class NotFoundError extends CustomError {}
 
 export class UnknownMemberError extends NotFoundError {
-    readonly name = 'NotfoundError'
     constructor(interaction: RepliableInteraction) {
         super(interaction.t('error:notfound:unknown_member'))
     }
 }
 
 export class UnknownEmojiError extends NotFoundError {
-    readonly name = 'UnknownEmojiError'
     constructor(interaction: RepliableInteraction) {
         super(interaction.t('error:notfound:unknown_emoji'))
     }
 }
 
 export class UnknownVoiceError extends NotFoundError {
-    readonly name = 'UnknownVoiceError'
     constructor(interaction: RepliableInteraction) {
         super(interaction.t('error:notfound:unknown_voice'))
     }
 }
 
 export class UnknownRoleError extends NotFoundError {
-    readonly name = 'UnknownRoleError'
     constructor(interaction: RepliableInteraction) {
         super(interaction.t('error:notfound:unknown_role'))
+    }
+}
+
+export class UnknownChannelError extends NotFoundError {
+    constructor(interaction: RepliableInteraction) {
+        super(interaction.t('error:notfound:unknown_channel'))
     }
 }
