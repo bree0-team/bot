629a2d88903de6bb987500f765a5fb0094332f73
Alexandr Musikhin <tophackr@icloud.com>
2024-07-11 12:33:15 +0300

    add clan dependency

--- src/modules/clan/ad/models/clan-ad-cooldown.model.ts
+++ src/modules/clan/ad/models/clan-ad-cooldown.model.ts
@@ -1,4 +1,5 @@
-import {AllowNull, Column, Model, PrimaryKey, Table} from 'sequelize-typescript'
+import {AllowNull, BelongsTo, Column, ForeignKey, Model, PrimaryKey, Table} from 'sequelize-typescript'
+import {ClanModel} from '../../models/clan.model.js'
 import {ClanId} from '../../types/clan.type.js'
 
 interface ClanAdCooldownAttrs {
@@ -10,10 +11,14 @@ interface ClanAdCooldownCreationAttrs extends ClanAdCooldownAttrs {}
 @Table({tableName: 'clan_ad_cooldown', updatedAt: false})
 export class ClanAdCooldownModel extends Model<ClanAdCooldownAttrs, ClanAdCooldownCreationAttrs> {
     @PrimaryKey
+    @ForeignKey(() => ClanModel)
     @Column
     clanId: ClanId
 
     @AllowNull(false)
     @Column
     updatedAt: Date
+
+    @BelongsTo(() => ClanModel)
+    clan: ReturnType<() => ClanModel>
 }
