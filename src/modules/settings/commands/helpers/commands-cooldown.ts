404a673a97fa1989610f678dfa643e4ed0c5d79a
Alexandr Musikhin <tophackr@icloud.com>
2024-07-11 13:41:59 +0300

    fix cooldown

--- src/modules/settings/commands/helpers/commands-cooldown.ts
+++ src/modules/settings/commands/helpers/commands-cooldown.ts
@@ -14,7 +14,7 @@ export async function commandsCooldown(command: CommandName, guildId: GuildId, u
     const {cooldown, cooldownType} = await SettingsCommandsManager.getOne(guildId, command)
     if (!cooldownType) return createCooldown(guildId, userId, command)
     const down = CommandsCooldownManager
-        .findOne(guildId, cooldownType === CooldownType.USER ? userId : undefined)
+        .findOne(guildId, command, cooldownType === CooldownType.USER ? userId : undefined)
     if (!down) return createCooldown(guildId, userId, command)
     const getTime = down.updatedAt.getTime() + (cooldown * 1000)
     if (getTime > new Date().getTime()) return time(Math.floor(getTime / 1000), 'R')
