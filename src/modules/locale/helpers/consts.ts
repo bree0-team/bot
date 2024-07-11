c9e6688c44182778df42fa44c53d3685ecac2698
Alexandr Musikhin <tophackr@icloud.com>
2024-07-11 14:06:02 +0300

    make new locale file

--- src/modules/locale/helpers/consts.ts
+++ src/modules/locale/helpers/consts.ts
@@ -1,28 +1,28 @@
 import {Locale} from 'discord.js'
+import {EmojiIdentifierResolvable} from 'discord.js/typings'
 import {ITranslate} from '../../../services/i18n.js'
-import {ValueOf} from '../../../types/base.type.js'
 
 export const SERVER_LANGUAGE = 'server_language'
-
-export enum BotLocale {
-	Belarusian = 'be'
+export enum AppLocale {
+	EnglishUS = Locale.EnglishUS,
+	Russian = Locale.Russian,
+	Ukrainian = Locale.Ukrainian
 }
-const AppLocale = {...Locale, ...BotLocale}
-export type AppLocaleValues = ValueOf<typeof AppLocale>
 
 export interface LanguageChoice {
-	emoji: string
+	emoji: EmojiIdentifierResolvable
 	name: string
-	value: AppLocaleValues | typeof SERVER_LANGUAGE
+	value: AppLocale | typeof SERVER_LANGUAGE
 }
 
+export const AppLocaleValues: LanguageChoice[] = [
+	{emoji: '🇺🇸', name: 'English, US', value: AppLocale.EnglishUS},
+	{emoji: '🇷🇺', name: 'Русский', value: AppLocale.Russian},
+	{emoji: '🇺🇦', name: 'Українська', value: AppLocale.Ukrainian}
+]
+
 export function languageChoices(t: ITranslate): LanguageChoice[] {
-	const arr: LanguageChoice[] = [
-		{emoji: '🇧🇾', name: 'Беларускі', value: BotLocale.Belarusian},
-		{emoji: '🇺🇸', name: 'English, US', value: Locale.EnglishUS},
-		{emoji: '🇷🇺', name: 'Русский', value: Locale.Russian},
-		{emoji: '🇺🇦', name: 'Українська', value: Locale.Ukrainian}
-	]
+	const arr: LanguageChoice[] = [...AppLocaleValues]
 	arr.sort((a, b) => (a.value < b.value ? -1 : 1))
 	arr.splice(0, 0, {
 		emoji: '🏳️', name: t('language:server_locale'), value: SERVER_LANGUAGE
