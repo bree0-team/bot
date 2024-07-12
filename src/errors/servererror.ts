9ec64f9b42a8b80dab3570d13fd901faf9c0fa80
Alexandr Musikhin <tophackr@icloud.com>
2024-07-12 10:18:42 +0300

    rename variable

--- src/errors/servererror.ts
+++ src/errors/servererror.ts
@@ -1,13 +1,10 @@
 import {RepliableInteraction} from 'discord.js'
 import {CustomError} from './general.js'
 
-abstract class ServerError extends CustomError {
-    public name = 'ServerError'
-}
+abstract class ServerError extends CustomError {}
 
 export class ValidUrlError extends ServerError {
-    readonly name = 'ValidUrlError'
     constructor(interaction: RepliableInteraction) {
-        super(interaction.t('error:forbidden:valid_url'))
+        super(interaction.t('error:servererror:valid_url'))
     }
 }
