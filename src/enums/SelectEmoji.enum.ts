8344af45f3765fe684e155ee3f142b825ab273aa
Alexandr Musikhin <tophackr@icloud.com>
2024-07-13 20:48:41 +0300

    move emojis and add new emojis

--- src/enums/SelectEmoji.enum.ts
+++ src/enums/SelectEmoji.enum.ts
@@ -1,4 +1,14 @@
 export enum SelectEmoji {
     Apps = '<:apps:1249675939944730696> ',
-    Command = '<:command:1234399238968836128>'
+    Command = '<:command:1234399238968836128>',
+    EmbedColor = '<:embedColor:1260187526090588191>',
+    ServerLanguage = '<:language:1261211994015531060>',
+    Ephemeral = '<:ephemeral:1261720469644644423>',
+    Manage = '<:manage:1260550798165938196>',
+    Transfer = '<:transfer:1260182840989978626>',
+    Appearance = '<:appearance:1249650541252837388>',
+    Limit = '<:limit:1249754437497655326>',
+    Channels = '<:channels:1249667983635255358>',
+    NamePattern = '<:namePattern:1249784557906038924>',
+    Role = '<:role:1260127366169104445>️'
 }
