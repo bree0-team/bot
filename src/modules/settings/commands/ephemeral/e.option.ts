f817939086795b5aa433c3ff93ed8c02dd8a1a3d
Alexandr Musikhin <tophackr@icloud.com>
2024-07-13 20:48:57 +0300

    add new emojis

--- src/modules/settings/commands/ephemeral/e.option.ts
+++ src/modules/settings/commands/ephemeral/e.option.ts
@@ -1,9 +1,10 @@
 import {SelectBuilder} from '../../../../builders/select.js'
+import {SelectEmoji} from '../../../../enums/SelectEmoji.enum.js'
 import {COMMAND_TYPE_SELECT, SettingsCommandsTypeSelectValuesCustomIds} from '../enums/CustomIds.enum.js'
 import {Ephemeral} from './Ephemeral.js'
 
 export default new SelectBuilder(COMMAND_TYPE_SELECT, Ephemeral, {
-    //emoji: SelectEmoji.Command,
+    emoji: SelectEmoji.Ephemeral,
     label: 'settings:commands:options:ephemeral',
     value: SettingsCommandsTypeSelectValuesCustomIds.Ephemeral
 })
--- src/modules/settings/roles/r.option.ts
+++ src/modules/settings/roles/r.option.ts
@@ -1,9 +1,10 @@
 import {SelectBuilder} from '../../../builders/select.js'
+import {SelectEmoji} from '../../../enums/SelectEmoji.enum.js'
 import {MAIN_SELECT, MainSelectValuesCustomIds} from '../enums/CustomIds.enum.js'
 import {Roles} from './Roles.js'
 
 export default new SelectBuilder(MAIN_SELECT, Roles, {
-    //emoji: SelectEmoji.Command,
+    emoji: SelectEmoji.Manage,
     label: 'settings:options:roles',
     value: MainSelectValuesCustomIds.Roles
 })
