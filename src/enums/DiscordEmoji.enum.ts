b6ce98a2cc51f6dcaef59fa8ada562f07ac5f7e1
Alexandr Musikhin <tophackr@icloud.com>
2024-07-15 10:03:56 +0300

    updated discord emoji

--- src/enums/DiscordEmoji.enum.ts
+++ src/enums/DiscordEmoji.enum.ts
@@ -1,6 +1,6 @@
 export enum DiscordEmoji {
-    TextChannel = '<:textChannel:1257004450183249940>',
-    VoiceChannel = '<:voiceChannel:1257004446051995649>',
-    CategoryChannel = '<:categoryChannel:1257004448631488604>',
-    Mention = '<:mention:1226237886580396122>'
+    TextChannel = '<:text:1261784843180838994>',
+    VoiceChannel = '<:voice:1261784841331150951>',
+    CategoryChannel = '<:category:1261981351045894214>',
+    Mention = '<:mention:1261784846619902074>'
 }
